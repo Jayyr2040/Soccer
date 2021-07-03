892ad5d8e5d717169785e5a1e7c59a26a005e5ec
jaytai2040 <jayyr2040@gmail.com>
2021-07-03 20:15:11 +0800

    add .env

--- src/WebUrl.js
+++ src/WebUrl.js
@@ -1,5 +1,5 @@
 const WebUrl = {
-    news: 'https://gnews.io/api/v4/top-headlines?q=manchester+united&token=46e1686127a8bab7d64f6af9e0b4a124&lang=en&max=1&in=title',
+    news: `https://gnews.io/api/v4/top-headlines?q=manchester+united&token=${process.env.REACT_APP__GNEWS_API_KEY}&lang=en&max=1&in=title`,
     highlights: 'https://www.scorebat.com/video-api/v1/'
 }
 
